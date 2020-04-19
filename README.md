4920edc03d54cf628ad0baf238c9b4bbb7345ead
Jason Le <le.kent.jason@gmail.com>
2020-04-19 19:13:23 -0400

    completion

---
+++ README.md
@@ -0,0 +1,24 @@
+# Jason Le's Cloudflare Workers Internship Application: Full-Stack
+
+### Notes
+
+After using a few different solutions from AWS lambda, STDLib and GCP Functions, I found that Cloudflare workers were extremely easy to use. The new `wrangler dev` made it easy to progressively build out the worker and test.
+
+## Extra Credits
+
+### Changing copy/URLs
+
+![image](example.png)
+
+- The HTMLRewriter API provided a neat way of modifying static content
+    - Using `element handlers`
+        - the title of the web page can be updated to `Routeflare`
+        - the main title on the page can be prepended with content
+        - the url's `href` attribute can be updated and text updated
+    - Using `text chunks` we can combine all chunks to count the characters
+
+### Deployment
+Using the wrangler environments, I can have two different deployments
+1. "Fake" Production https://jasonle.org/routeflare
+
+2. "Fake" Development on workers.dev http.s://routeflare.jqwotos.workers.dev/
---
+++ index.js
@@ -0,0 +1,109 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Respond with one of two variants
+ * @param request
+ * @returns {Promise<Response>}
+ */
+async function handleRequest(request) {
+  let variant;
+  let init = {
+    headers: {
+      'content-type': 'text/html'
+    }
+  };
+  const cookieVariant = findCookie(request);
+
+  if (cookieVariant) {
+    variant = cookieVariant;
+  } else {
+    const variantOptions = (await getVariants())["variants"];
+    const selectedVariant = Math.random() < 0.5 ? variantOptions[0] : variantOptions[1];
+    init.headers['Set-Cookie'] = `variant=${selectedVariant}`;
+    variant = selectedVariant;
+  }
+
+  let response = await fetch(variant, init);
+  response = rewriter.transform(response);
+  return new Response(await response.text(), init);
+}
+
+/**
+ * Fetch variant options
+ * @returns {Promise<any>}
+ */
+async function getVariants() {
+  const request = await fetch(variantURL);
+  return request.json();
+}
+
+/**
+ * Attemps to find the persisting variant
+ *
+ * @param request
+ * @returns {(string|null)} The variant if it exists, or null
+ */
+function findCookie(request) {
+  let result = null;
+  const cookieString = request.headers.get('cookie');
+  if (cookieString) {
+    let cookieSplit = cookieString.split(';');
+    cookieSplit.forEach(cookie => {
+      let [cookieName, cookieValue] = cookie.split('=');
+      cookieName = cookieName.trim();
+      if (cookieName === 'variant') {
+        result = cookieValue;
+      }
+    })
+  }
+  return result;
+}
+
+class TitleRewriter {
+  element(element) {
+    element.setInnerContent(newTitle);
+  }
+}
+
+class MainTitleRewriter {
+  element(element) {
+    element.prepend(prependMainTitle);
+  }
+}
+
+class DescriptionRewriter {
+  constructor() {
+    this.buffer = "";
+  }
+
+  text(txt) {
+    this.buffer += txt.text
+    if (txt.lastInTextNode) {
+      txt.after(`(${this.buffer.length} characters)`);
+      this.buffer = "";
+    }
+  }
+}
+
+class LinkRewriter {
+  element(element) {
+    element.setAttribute('href', newLink.link)
+    element.setInnerContent(newLink.content);
+  }
+}
+
+const rewriter = new HTMLRewriter()
+    .on('title', new TitleRewriter())
+    .on('h1#title', new MainTitleRewriter())
+    .on('p#description', new DescriptionRewriter())
+    .on('a#url', new LinkRewriter());
+
+const newTitle = "Routeflare";
+const prependMainTitle = "Routeflare: ";
+const newLink = {
+  link: 'https://github.com/jqwotos',
+  content: 'Visit my Github!'
+}
+const variantURL = "https://cfw-takehome.developers.workers.dev/api/variants";
